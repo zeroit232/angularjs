98d9c2e2a79effc81bc49d645cfbebcbab4f63f7
hominhquan <minhquan232@gmail.com>
2014-07-08 16:44:12 +0700

    Change project structure

--- src/main/resources/static/app/js/controllers/aboutCtrl.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by zero_it on 7/4/2014.
- */
-angular.module('starterApp.about', [])
-    .controller('AboutCtrl', function() {
-    });
--- src/main/resources/static/app/js/controllers/homeCtrl.js
+++
@@ -1,3 +0,0 @@
-angular.module('starterApp.home', [])
-    .controller('HomeCtrl', function() {
-    });
